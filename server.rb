4d558f2fcf5cb4f5a09addf792875fc29587daf3
Yutaka HARA <yutaka.hara+github@gmail.com>
2009-03-20 14:04:13 +0900

    new: server by ramaze

---
+++ server.rb
@@ -0,0 +1,13 @@
+require 'rubygems'
+require 'ramaze'
+
+class MainController < Ramaze::Controller
+  def index
+  end
+end
+
+#class TicktesController < Ramaze::Controller
+#  map '/tickets'
+#end
+
+Ramaze.start
