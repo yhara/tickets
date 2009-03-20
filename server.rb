d1e6c4f2581957623b13371fab53a9a429a358c0
Yutaka HARA <yutaka.hara+github@gmail.com>
2009-03-20 16:32:29 +0900

    new: list tickets as s-exp

--- server.rb
+++ server.rb
@@ -9,11 +9,17 @@ end
 
 class TicktesController < Ramaze::Controller
   map '/tickets'
+  deny_layout :all
 
   def list
     #Ticket.new(:title => "foo", :importance => 0, :emergency => 0).save
     #Ramaze::Log.debug Ticket.new(:title => "bar").save
-    Ticket.all.inspect
+
+    '(' +
+    Ticket.all.map{|ticket|
+      "(#{ticket.title} #{ticket.importance} #{ticket.emergency})"
+    }.join(' ') +
+    ')'
   end
 end
 
