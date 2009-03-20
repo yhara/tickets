abd39934fc4abbf61ce5510e658a102e6c42418e
Yutaka HARA <yutaka.hara+github@gmail.com>
2009-03-20 15:45:11 +0900

    new: list tickets by /tickets/list

--- server.rb
+++ server.rb
@@ -1,13 +1,20 @@
 require 'rubygems'
 require 'ramaze'
+require 'model/ticket'
 
 class MainController < Ramaze::Controller
   def index
   end
 end
 
-#class TicktesController < Ramaze::Controller
-#  map '/tickets'
-#end
+class TicktesController < Ramaze::Controller
+  map '/tickets'
+
+  def list
+    #Ticket.new(:title => "foo", :importance => 0, :emergency => 0).save
+    #Ramaze::Log.debug Ticket.new(:title => "bar").save
+    Ticket.all.inspect
+  end
+end
 
 Ramaze.start
