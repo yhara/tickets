b31c00988bcb1686f55766dba5db24c3cc2f2789
Yutaka HARA <yutaka.hara+github@gmail.com>
2009-03-23 17:01:42 +0900

    change: extract scheme source to main.scm

---
+++ public/scm/main.scm
@@ -0,0 +1,50 @@
+;;;
+;;; main.scm
+;;;
+
+;; load
+(for-each (lambda (pair)
+            (js-load (string-append "js/" (car pair) ".js")
+                     (cdr pair)))
+          '(("scriptaculous" . "Scriptaculous")
+            ("builder" . "Builder")
+            ("effects" . "Effect")
+            ("dragdrop" . "Draggable")
+            ("controls" . "Autocompleter")
+            ("slider" . "Control")
+            ("sound" . "Sound")))
+
+(load "scm/ticket.scm")
+
+;; consts
+
+(define (px n)
+  (string-append (number->string n) "px"))
+
+(define (set-position! elem x y)
+  (set-style! elem "position" "absolute") 
+  (set-style! elem "left" (px x))
+  (set-style! elem "top" (px y)))
+
+(define (get-position elem)
+  (define (remove-px str)
+    (string->number (car (regexp-exec "(\\d+)" str))))
+  (values (remove-px (get-style elem "left"))
+          (remove-px (get-style elem "top"))))
+
+(define *width* 600)
+(define *height* 600)
+
+(set-style! ($ "field") "width" (px *width*))
+(set-style! ($ "field") "height" (px *height*))
+(set-position! ($ "top")    (/ *width* 2) 0)
+(set-position! ($ "bottom") (/ *width* 2) *height*)
+(set-position! ($ "right")  *width* (/ *height* 2))
+(set-position! ($ "left")   0       (/ *height* 2))
+
+(define show-error print)
+
+(for-each (lambda (vals) (apply ticket-new! vals))
+          (read-from-string (http-request "tickets/list")))
+
+(display "ok")
