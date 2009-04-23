d13b8293e1d1459daa42e5510ae212120893e422
Yutaka HARA <yutaka.hara+github@gmail.com>
2009-04-23 17:38:55 +0900

    new: sspec (simple unit test tool)

---
+++ public/scm/test.scm
@@ -0,0 +1,46 @@
+;;
+;; test.scm
+;;
+
+(define *escape* (call/cc (lambda (x) x)))
+(define (raise msg)
+  (print "ERROR: " msg)
+  (*escape*))
+
+;;; sspec
+
+; (expect expr)
+; (expect expr is value) -> checks (eq? result value)
+(define-macro (expect . args)
+  (case (length args)
+    ((1)
+     `(sspec-check ,(car args) "result is false" ',args))
+    ((3)
+     (when (not (eq? (cadr args) 'is))
+       (raise "expect: bad args"))
+     `(let ((__given__    ,(car args))
+            (__expected__ ,(caddr args)))
+        (sspec-check2 __given__ __expected__ ',args)))
+    (else
+      (raise "expect: bad args"))))
+
+(define (sspec-check result message-gen args)
+  (if result
+    (print "ok : " (write-to-string args))
+    (print "ng[" (message-gen) "] : " (write-to-string args))))
+
+(define (sspec-check2 given expected args)
+  (sspec-check (eq? given expected)
+               (lambda ()
+                 (string-append "expected "
+                                (write-to-string expected)
+                                " but got "
+                                (write-to-string given)))
+               args))
+
+
+;;; test body
+
+(expect (= 1 1))
+(define x 1)
+(expect x is 2)
